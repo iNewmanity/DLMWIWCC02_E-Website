5f0f303c37f8f196b4c752a7c95bc5ebdd11a2c3
Jan Neumann <janneumannnneisen@gmail.com>
2026-01-02 15:14:55 +0100

    feat(shadcn): Add Header to Page Layout

--- dlmwiwcc01_e/src/routes/+layout.svelte
+++ dlmwiwcc01_e/src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.png';
+    import Navigation from "$lib/components/blocks/navigation/navigation.svelte";
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<div class="mt-4 mr-4 ml-4">
+    <Navigation/>
+</div>
+<div class="mr-4 ml-4">
+    {@render children()}
+</div>
+
