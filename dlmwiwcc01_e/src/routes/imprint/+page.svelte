25cf0d6444cfff7dc3a5006df482d0946d750814
Jan Neumann <janneumannnneisen@gmail.com>
2026-01-09 16:36:52 +0100

    feat(utility): Added a date formatter function to a frontend utilities service

--- dlmwiwcc01_e/src/routes/imprint/+page.svelte
+++ dlmwiwcc01_e/src/routes/imprint/+page.svelte
@@ -2,9 +2,13 @@
     import Markdown from '$lib/components/custom/markdown/markdown.svelte';
     import type { PageProps } from './$types';
 
+    import { formatDate } from '$lib/services/utilities.service';
+
     let { data }: PageProps = $props();
 
     let articles = $derived(data.contents);
+
+
 </script>
 
 <div class="m-4">
@@ -12,7 +16,7 @@
         {articles[0].title}
     </h1>
     <p class="text-muted-foreground text-l leading-7">
-        Last Updated: {articles[0].released}
+        Last Updated: {formatDate(articles[0].released)}
     </p>
     <Markdown markdown={articles[0].text} />
 
