9722595d0c3c154491141fc0fd4f6b4df5e2b284
Jan Neumann <janneumannnneisen@gmail.com>
2026-01-11 14:32:15 +0100

    feat(cache): Added Cache for the image of the landing page to save to bucket bandwidth

--- dlmwiwcc01_e/src/routes/api/image/[fileId]/+server.ts
+++ dlmwiwcc01_e/src/routes/api/image/[fileId]/+server.ts
@@ -1,9 +1,22 @@
 import {error} from "@sveltejs/kit";
 import type {RequestHandler} from "./$types";
 import {buildBucketURI} from "$lib/server/media/media";
+import {type CachedImage, imageCache} from "$lib/server/cache/cache"
 
 export const GET: RequestHandler = async ({params, fetch}) => {
     const {fileId} = params;
+
+    const cached = imageCache.get<CachedImage>(fileId);
+
+    if (cached) {
+        return new Response(cached.data as any, {
+            headers: {
+                'Content-Type': cached.contentType,
+                'X-Cache': 'HIT',
+                'Cache-Control': 'public, max-age=31536000'
+            }
+        });
+    }
 
     const url = buildBucketURI(fileId)
 
@@ -13,12 +26,16 @@ export const GET: RequestHandler = async ({params, fetch}) => {
         if (!response.ok) {
             throw error(response.status, 'Image could not be loaded');
         }
+        const contentType = response.headers.get('Content-Type') || 'image/png';
+        const arrayBuffer = await response.arrayBuffer();
+        const data = Buffer.from(arrayBuffer);
 
-        const blob = await response.blob();
+        imageCache.set(fileId, {data, contentType});
 
-        return new Response(blob, {
+        return new Response(data as any, {
             headers: {
-                'Content-Type': response.headers.get('Content-Type') || 'image/png',
+                'Content-Type': contentType,
+                'X-Cache': 'MISS',
                 'Cache-Control': 'public, max-age=31536000'
             }
         });
